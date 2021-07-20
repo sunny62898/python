bc8bbc6cfb74910f1dec69373403d300af6fae8b
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-07-20 15:45:36 +0800

    begin to learn deep learning

---
+++ DeepLearning/DeepLearning1.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 14:23:20 2021
+
+@author: User
+"""
+
+import torch
+import numpy as np
+
+data = [[1,2],[3,4]]
+x_data = torch.tensor(data)   #宣告張量跟data一樣的torch
+print(f"{data}")
+print(x_data)
+
+np_array = np.array(data)  #建立一個跟data一樣的ndarray
+x_np = torch.from_numpy(np_array)   #將ndarray轉成torch型態
+
+print(x_np)
+
+x_ones = torch.ones_like(x_data)
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype = torch.float)
+print(f"Random Tensor: \n {x_rand} \n")
+
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+print(f"Random Tensor: \n {rand_tensor} \n")
+
+ones_tensor = torch.ones(shape)
+print(f"Ones Tensor: \n {ones_tensor} \n")
+
+zeros_tensor = torch.zeros(shape)
+print(f"Zeros Tensor: \n {zeros_tensor} \n")
+
+tensor = torch.rand(3,4)
+print(tensor)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+#這裡轉換CPU到GPU的概念不懂
+if torch.cuda.is_available():
+    tensor = tensor.to('cuda')
+
+print(f"Device tensor move to stored on: {tensor.device}")
+#end
+
+tensor = torch.ones(4,4)
+tensor[:,1] = 0  #將第1行換成0
+print(tensor)
+
+tensor[1,:] = 0  #將第1列換成0
+print(tensor)
+
+t1 = torch.cat([tensor, tensor, tensor], dim = 1)  #橫的接起來
+print(t1)
+
+
+
+
+
