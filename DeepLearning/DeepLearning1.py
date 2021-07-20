2afbbb654d2cee38944462ecc69e95d5153ca28c
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-07-20 22:25:17 +0800

    finished tensor

--- DeepLearning/DeepLearning1.py
+++ DeepLearning/DeepLearning1.py
@@ -57,6 +57,45 @@ print(tensor)
 t1 = torch.cat([tensor, tensor, tensor], dim = 1)  #橫的接起來
 print(t1)
 
+#乘法
+#對應位置的矩陣相乘  所以兩個的維度要相同
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+#矩陣乘一個數值
+print(f"tensor * tensor \n {tensor * tensor}")
+
+#計算兩個矩陣相乘
+print(f"tensor.T \n {tensor.T}")
+print(f"tensor.matmul(tensor.T) \n {tensor.matmul(tensor.T)} \n")
+
+print(f"tensor @ tensor.T \n {tensor @ tensor.T}")
+
+
+print(x_data.T)  #.T 轉置矩陣
+
+print(tensor , "\n")
+tensor.add_(5)
+print(tensor)
+
+
+#tensor 轉成 numpy
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(5)  #當原本的tensor加上數字時numpy也會跟著變動
+print(f"t: {t}")
+print(f"n: {n}")
+
+#numpy array 轉成 tensor
+n2 = np.ones(5)
+t2 = torch.from_numpy(n2)
+print(f"n2: {n2}")
+print(f"t2: {t2}")
+
+np.add(n2, 1, out = n2)  #np.add(要被加的, 加多少, 加完存到哪個變數)
+print(f"n2: {n2}")
+print(f"t2: {t2}")  #當numpy array被加tensor也會跟著被更動
 
 
 
